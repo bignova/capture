72e528164218168bc90bfa9ab832f5760acf5d77
Yimeng Wu <wuym0404@gmail.com>
2021-02-01 16:51:25 +0800

    add page transition animations

--- src/App.js
+++ src/App.js
@@ -8,19 +8,25 @@ import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
 import MovieDetail from "./pages/MovieDetail";
 // Router
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
+// Animation
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route exact path="/" component={AboutUs} />
-        <Route exact path="/work" component={OurWork} />
-        <Route path="/work/:id" component={MovieDetail} />
-        <Route path="/contact" component={ContactUs} />
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route exact path="/" component={AboutUs} />
+          <Route exact path="/work" component={OurWork} />
+          <Route path="/work/:id" component={MovieDetail} />
+          <Route path="/contact" component={ContactUs} />
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
