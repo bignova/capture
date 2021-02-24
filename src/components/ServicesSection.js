41e237a1fbf95690030b80fc5c7b453cda1931cf
Yimeng Wu <wuym0404@gmail.com>
2021-02-24 17:12:23 +0800

    mobile friendly

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -76,6 +76,9 @@ const Services = styled(About)`
 const Cards = styled.div`
   display: flex;
   flex-wrap: wrap;
+  @media (max-width: 960px) {
+    justify-content: center;
+  }
 `;
 
 const Card = styled.div`
